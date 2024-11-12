0786efa80a8ee7955a0759e7984d4d9a6fe815ea
Martin Nessa <martinnessa7@gmail.com>
2024-11-12 20:27:03 +0100

    First commit

---
+++ mane041-image-generation/hello_world/app.py
@@ -0,0 +1,50 @@
+import base64
+import os
+import boto3
+import json
+import random
+
+
+def lambda_handler(event, context):
+
+    body = json.loads(event["body"])
+    prompt = body["prompt"]
+
+    bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
+    s3_client = boto3.client("s3")
+
+    # Define the model ID and S3 bucket name (replace with your actual bucket name)
+    model_id = "amazon.titan-image-generator-v1"
+    bucket_name = os.environ["bucket_name"]
+
+    seed = random.randint(0, 2147483647)
+    s3_image_path = f"17/generated_images/titan_{seed}.png"
+
+    native_request = {
+        "taskType": "TEXT_IMAGE",
+        "textToImageParams": {"text": prompt},
+        "imageGenerationConfig": {
+            "numberOfImages": 1,
+            "quality": "standard",
+            "cfgScale": 8.0,
+            "height": 1024,
+            "width": 1024,
+            "seed": seed,
+        }
+    }
+
+    response = bedrock_client.invoke_model(modelId=model_id, body=json.dumps(native_request))
+    model_response = json.loads(response["body"].read())
+
+    # Extract and decode the Base64 image data
+    base64_image_data = model_response["images"][0]
+    image_data = base64.b64decode(base64_image_data)
+
+    # Upload the decoded image data to S3
+    s3_client.put_object(Bucket=bucket_name, Key=s3_image_path, Body=image_data)
+
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Image generation initiated", "prompt": prompt})
+    }
