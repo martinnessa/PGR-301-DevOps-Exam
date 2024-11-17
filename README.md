138bef2d1d8594ea93e0f6293336feb1ed2b7dfd
Martin Nessa <martinnessa7@gmail.com>
2024-11-17 12:30:23 +0100

    remove unused stuff

---
+++ README.md
@@ -0,0 +1,7 @@
+Oppgave 1:
+
+A: Endpoint til Lambda funksjon: https://0v2ids1yv8.execute-api.eu-west-1.amazonaws.com/Prod/generate-image
+
+B:  
+Kjørt pipeline: https://github.com/martinnessa/PGR-301-DevOps-Exam/actions/runs/11824568856/job/32946357720
+
--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,31 @@
 terraform {
-  required_version = ">= 1.0.0" # Ensure that the Terraform version is 1.0.0 or higher
+  required_version = ">= 1.9"
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws" # Specify the source of the AWS provider
-      version = "~> 4.0"        # Use a version of the AWS provider that is compatible with version
+      source = "hashicorp/aws" # Specify the source of the AWS provider
+      version = "~> 5.74.0"        # Use a version of the AWS provider that is compatible with version
     }
+  }
+
+  backend "s3" {
+    bucket = "pgr301-2024-terraform-state"
+    key = "17/terraform.tfstate"
+    region = "eu-west-1"
   }
 }
 
 provider "aws" {
-  region = "us-east-1" # Set the AWS region to US East (N. Virginia)
+  region = "eu-west-1"
 }
 
-resource "aws_instance" "aws_example$END$" {
+resource "aws_lambda_function" "mane041_sqs_image_generation" {
   tags = {
     Name = "ExampleInstance" # Tag the instance with a Name tag for easier identification
   }
+  function_name = "mane041_sqs_image_generation"
+}
+
+resource "aws_iam_policy" "lambda_role" {
+  policy = ""
 }
