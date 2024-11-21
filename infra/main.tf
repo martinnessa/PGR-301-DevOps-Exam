a9df6b95a7acf96701cdb5ee886ba4cc592de2fb
Martin Nessa <martinnessa7@gmail.com>
2024-11-21 09:17:35 +0100

    Task 2

--- infra/main.tf
+++ infra/main.tf
@@ -3,14 +3,14 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws" # Specify the source of the AWS provider
-      version = "~> 5.74.0"        # Use a version of the AWS provider that is compatible with version
+      source  = "hashicorp/aws" # Specify the source of the AWS provider
+      version = "~> 5.74.0"     # Use a version of the AWS provider that is compatible with version
     }
   }
 
   backend "s3" {
     bucket = "pgr301-2024-terraform-state"
-    key = "17/terraform.tfstate"
+    key    = "17/terraform.tfstate"
     region = "eu-west-1"
   }
 }
@@ -20,12 +20,44 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "mane041_sqs_image_generation" {
-  tags = {
-    Name = "ExampleInstance" # Tag the instance with a Name tag for easier identification
-  }
+  filename      = "lambda_sqs.py"
   function_name = "mane041_sqs_image_generation"
+  runtime       = "python3.12"
+  role          = "aws_iam_role.mane041_lambda_role.arn"
+
+  environment {
+    variables = {
+      BUCKET_NAME      = "pgr301-couch-explorers"
+      CANDIDATE_NUMBER = "17"
+    }
+  }
 }
 
-resource "aws_iam_policy" "lambda_role" {
-  policy = ""
+resource "aws_sqs_queue" "mane041_sqs_queue" {
+
+}
+
+resource "aws_iam_policy" "mane041_lambda_policy" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+        ]
+        Resource = "arn:aws:s3:::pgr301-couch-explorers"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "bedrock:InvokeModel"
+        Resource = "arn:aws:bedrock:us-east-1"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "mane041_lambda_role" {
+  assume_role_policy = ""
 }
